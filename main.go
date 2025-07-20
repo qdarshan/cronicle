005fb6901f067caf426e82d4b5efa9ec99ce4c6a
darshan <darundarshan1223@gmail.com>
2025-07-20 18:35:43 +0530

    feat: handle day of week field

--- main.go
+++ main.go
@@ -22,17 +22,20 @@ func main() {
 	}
 
 	fmt.Println("-- Minute --")
+	fmt.Println("Input: 1         →", validateMinute("1"))
 	fmt.Println("Input: 1,2       →", validateMinute("1,2"))
 	fmt.Println("Input: 1-8       →", validateMinute("1-8"))
 	fmt.Println("Input: *         →", validateMinute("*"))
 	fmt.Println("Input: 4/9       →", validateMinute("4/9"))
 
 	fmt.Println("-- Hour --")
+	fmt.Println("Input: 1         →", validateHour("1"))
 	fmt.Println("Input: 1,2,13    →", validateHour("1,2,13"))
 	fmt.Println("Input: 0/2       →", validateHour("0/2"))
 	fmt.Println("Input: 9-17      →", validateHour("9-17"))
 
 	fmt.Println("-- Day of Month --")
+	fmt.Println("Input: 1         →", validateDayOfMonth("1"))
 	fmt.Println("Input: 1,2,13    →", validateDayOfMonth("1,2,13"))
 	fmt.Println("Input: 1/2       →", validateDayOfMonth("1/2"))
 	fmt.Println("Input: 3/5       →", validateDayOfMonth("3/5"))
@@ -40,6 +43,7 @@ func main() {
 	// fmt.Println("Input: L         →", validateDayOfMonth("L"))
 
 	fmt.Println("-- Month --")
+	fmt.Println("Input: 1         →", validateMonth("1"))
 	fmt.Println("Input: 1,2,11    →", validateMonth("1,2,11"))
 	fmt.Println("Input: 1/3       →", validateMonth("1/3"))
 	fmt.Println("Input: 4/2       →", validateMonth("4/2"))
@@ -47,11 +51,17 @@ func main() {
 	fmt.Println("Input: JAN,MAR   →", validateMonth("JAN,MAR"))
 	fmt.Println("Input: JUL-OCT   →", validateMonth("JUL-OCT"))
 
+	fmt.Println("-- Day of Week --")
+	fmt.Println("Input: MON-WED   →", validateDayOfWeek("MON-WED"))
+	fmt.Println("Input: 1-5       →", validateDayOfWeek("1-5"))
+	fmt.Println("Input: 1,3,5     →", validateDayOfWeek("1,3,5"))
+	fmt.Println("Input: 0         →", validateDayOfWeek("0"))
+
 }
 
 func validateMinute(minuteField string) string {
 	if strings.Contains(minuteField, "*") {
-		return "every minute"
+		return "Every minute"
 	} else if strings.Contains(minuteField, ",") {
 		values := strings.Split(minuteField, ",")
 		str := ""
@@ -66,12 +76,12 @@ func validateMinute(minuteField string) string {
 		return "At " + str + " minutes past the hour"
 	} else if strings.Contains(minuteField, "/") {
 		stepStart, stepInterval := handleStep(minuteField)
-		return "every " + strconv.Itoa(stepInterval) + " minutes, starting at " + strconv.Itoa(stepStart) + " minutes past the hour"
+		return "Every " + strconv.Itoa(stepInterval) + " minutes, starting at " + strconv.Itoa(stepStart) + " minutes past the hour"
 	} else if strings.Contains(minuteField, "-") {
 		rangeStart, rangeEnd := handleRange(minuteField)
 		return "Minutes " + strconv.Itoa(rangeStart) + " through " + strconv.Itoa(rangeEnd) + " past the hour"
 	}
-	return "every " + minuteField
+	return "At " + minuteField + " minutes past the hour"
 }
 
 func validateHour(hourField string) string {
@@ -94,12 +104,12 @@ func validateHour(hourField string) string {
 		return "At " + str
 	} else if strings.Contains(hourField, "/") {
 		stepStart, stepInterval := handleStep(hourField)
-		return "every " + strconv.Itoa(stepInterval) + " hours, starting at " + convert24To12(strconv.Itoa(stepStart))
+		return "Every " + strconv.Itoa(stepInterval) + " hours, starting at " + convert24To12(strconv.Itoa(stepStart))
 	} else if strings.Contains(hourField, "-") {
 		rangeStart, rangeEnd := handleRange(hourField)
 		return "between " + convert24To12(strconv.Itoa(rangeStart)) + " and " + convert24To12(strconv.Itoa(rangeEnd))
 	}
-	return "every " + hourField
+	return "between " + convert24To12(hourField) + " and " + convert24To12(hourField)
 }
 
 func validateDayOfMonth(dayOfMonthField string) string {
@@ -119,9 +129,9 @@ func validateDayOfMonth(dayOfMonthField string) string {
 	} else if strings.Contains(dayOfMonthField, "/") {
 		stepStart, stepInterval := handleStep(dayOfMonthField)
 		if stepStart == 1 {
-			return "every " + strconv.Itoa(stepInterval) + " days"
+			return "Every " + strconv.Itoa(stepInterval) + " days"
 		}
-		return "every " + strconv.Itoa(stepInterval) + " days, starting on day " + strconv.Itoa(stepStart) + " of the month"
+		return "Every " + strconv.Itoa(stepInterval) + " days, starting on day " + strconv.Itoa(stepStart) + " of the month"
 	} else if strings.Contains(dayOfMonthField, "-") {
 		rangeStart, rangeEnd := handleRange(dayOfMonthField)
 		return "between day " + strconv.Itoa(rangeStart) + " and " + strconv.Itoa(rangeEnd) + " of the month"
@@ -153,18 +163,48 @@ func validateMonth(monthField string) string {
 			}
 			str += ", "
 		}
-		return "only in " + str
+		return "Only in " + str
 	} else if strings.Contains(monthField, "/") {
 		stepStart, stepInterval := handleStep(monthField)
 		if stepStart == 1 {
-			return "every " + strconv.Itoa(stepInterval) + " months"
+			return "Every " + strconv.Itoa(stepInterval) + " months"
 		}
-		return "every " + strconv.Itoa(stepInterval) + " months, from " + months[stepStart-1] + " through DECEMBER"
+		return "Every " + strconv.Itoa(stepInterval) + " months, from " + months[stepStart-1] + " through DECEMBER"
 	} else if strings.Contains(monthField, "-") {
 		rangeStart, rangeEnd := handleRange(monthField)
 		return months[rangeStart-1] + " through " + months[rangeEnd-1]
 	}
-	return months[normalizeValue(monthField)-1]
+	return "Only in " + months[normalizeValue(monthField)-1]
+}
+
+func validateDayOfWeek(dayOfWeekField string) string {
+	weeks := []string{"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"}
+
+	if strings.Contains(dayOfWeekField, ",") {
+		values := strings.Split(dayOfWeekField, ",")
+		str := ""
+
+		for index := range values {
+			values[index] = weeks[normalizeValue(values[index])]
+		}
+
+		for index, value := range values {
+			str += value
+			if index == len(values)-2 {
+				str += " and " + values[index+1]
+				break
+			}
+			str += ", "
+		}
+		return "Only on " + str
+	} else if strings.Contains(dayOfWeekField, "/") {
+		stepStart, stepInterval := handleStep(dayOfWeekField)
+		return "Every " + strconv.Itoa(stepInterval) + " days of the week, " + weeks[stepStart] + " through DECEMBER"
+	} else if strings.Contains(dayOfWeekField, "-") {
+		rangeStart, rangeEnd := handleRange(dayOfWeekField)
+		return weeks[rangeStart] + " through " + weeks[rangeEnd]
+	}
+	return "Only on " + weeks[normalizeValue(dayOfWeekField)]
 }
 
 func convert24To12(hr string) string {
