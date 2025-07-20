147d5b4b84bf7265267ffc882fe89b461d59b737
darshan <darundarshan1223@gmail.com>
2025-07-20 13:56:19 +0530

    feat: handle month field

--- main.go
+++ main.go
@@ -1,35 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 )
 
+// unix corn
 func main() {
 	cronExpression := "5 12 1 4 4"
 	// fmt.Println(cronExpression)
 
 	if cronExpression == "" {
-		err := errors.New("invalid expression")
-		fmt.Println(err)
+		log.Fatal("invalid expression")
 	}
 
 	cronFields := strings.Split(cronExpression, " ")
-
 	if len(cronFields) != 5 {
-		err := errors.New("currently supports 5 standard cron fields")
-		fmt.Println(err)
+		log.Fatal("currently supports 5 standard cron fields")
 	}
-
-	// minuteField := cronFields[0]
-	// hourField := cronFields[1]
-	// datOfMonthField := cronFields[2]
-	// monthField := cronFields[3]
-	// dayOfWeekField := cronFields[4]
-	handleRange("JAN-DEC")
-	handleRange("MON-WED")
 
 	fmt.Println("-- Minute --")
 	fmt.Println("Input: 1,2       →", validateMinute("1,2"))
@@ -45,7 +35,18 @@ func main() {
 	fmt.Println("-- Day of Month --")
 	fmt.Println("Input: 1,2,13    →", validateDayOfMonth("1,2,13"))
 	fmt.Println("Input: 1/2       →", validateDayOfMonth("1/2"))
+	fmt.Println("Input: 3/5       →", validateDayOfMonth("3/5"))
 	fmt.Println("Input: 9-17      →", validateDayOfMonth("9-17"))
+	// fmt.Println("Input: L         →", validateDayOfMonth("L"))
+
+	fmt.Println("-- Month --")
+	fmt.Println("Input: 1,2,11    →", validateMonth("1,2,11"))
+	fmt.Println("Input: 1/3       →", validateMonth("1/3"))
+	fmt.Println("Input: 4/2       →", validateMonth("4/2"))
+	fmt.Println("Input: 5-8       →", validateMonth("5-8"))
+	fmt.Println("Input: JAN,MAR   →", validateMonth("JAN,MAR"))
+	fmt.Println("Input: JUL-OCT   →", validateMonth("JUL-OCT"))
+
 }
 
 func validateMinute(minuteField string) string {
@@ -101,9 +102,9 @@ func validateHour(hourField string) string {
 	return "every " + hourField
 }
 
-func validateDayOfMonth(dayOfMonth string) string {
-	if strings.Contains(dayOfMonth, ",") {
-		values := strings.Split(dayOfMonth, ",")
+func validateDayOfMonth(dayOfMonthField string) string {
+	if strings.Contains(dayOfMonthField, ",") {
+		values := strings.Split(dayOfMonthField, ",")
 		str := ""
 
 		for index, value := range values {
@@ -115,27 +116,67 @@ func validateDayOfMonth(dayOfMonth string) string {
 			str += ", "
 		}
 		return "on day " + str + " of the month"
-	} else if strings.Contains(dayOfMonth, "/") {
-		stepStart, stepInterval := handleStep(dayOfMonth)
+	} else if strings.Contains(dayOfMonthField, "/") {
+		stepStart, stepInterval := handleStep(dayOfMonthField)
 		if stepStart == 1 {
 			return "every " + strconv.Itoa(stepInterval) + " days"
 		}
 		return "every " + strconv.Itoa(stepInterval) + " days, starting on day " + strconv.Itoa(stepStart) + " of the month"
-	} else if strings.Contains(dayOfMonth, "-") {
-		rangeStart, rangeEnd := handleRange(dayOfMonth)
+	} else if strings.Contains(dayOfMonthField, "-") {
+		rangeStart, rangeEnd := handleRange(dayOfMonthField)
 		return "between day " + strconv.Itoa(rangeStart) + " and " + strconv.Itoa(rangeEnd) + " of the month"
 	}
-	return "on day " + dayOfMonth + " of the month"
+
+	// else if dayOfMonthField == "L" {
+	// 	return "on the last day of the month"
+	// }
+	return "on day " + dayOfMonthField + " of the month"
+}
+
+func validateMonth(monthField string) string {
+	months := []string{"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
+		"JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"}
+
+	if strings.Contains(monthField, ",") {
+		values := strings.Split(monthField, ",")
+		str := ""
+
+		for index := range values {
+			values[index] = months[normalizeValue(values[index])-1]
+		}
+
+		for index, value := range values {
+			str += value
+			if index == len(values)-2 {
+				str += " and " + values[index+1]
+				break
+			}
+			str += ", "
+		}
+		return "only in " + str
+	} else if strings.Contains(monthField, "/") {
+		stepStart, stepInterval := handleStep(monthField)
+		if stepStart == 1 {
+			return "every " + strconv.Itoa(stepInterval) + " months"
+		}
+		return "every " + strconv.Itoa(stepInterval) + " months, from " + months[stepStart-1] + " through DECEMBER"
+	} else if strings.Contains(monthField, "-") {
+		rangeStart, rangeEnd := handleRange(monthField)
+		return months[rangeStart-1] + " through " + months[rangeEnd-1]
+	}
+	return months[normalizeValue(monthField)-1]
 }
 
 func convert24To12(hr string) string {
 	i, _ := strconv.Atoi(hr)
-	if i < 12 {
-		return strconv.Itoa(i) + ":00 AM"
-	} else {
-		i %= 12
-		return strconv.Itoa(i) + ":00 PM"
+	if i == 0 {
+		return "12:00 AM"
+	} else if i == 12 {
+		return "12:00 PM"
+	} else if i < 12 {
+		return fmt.Sprintf("%d:00 AM", i)
 	}
+	return fmt.Sprintf("%d:00 PM", i-12)
 }
 
 func handleStep(field string) (int, int) {
@@ -150,13 +191,13 @@ func handleRange(field string) (int, int) {
 
 func normalizeValue(value string) int {
 	months := []string{"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}
-	weeks := []string{"SUN", "MON", "TUE", "WED", "THU", "FRI"}
+	weeks := []string{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"}
 
 	normalize, err := strconv.Atoi(value)
 	if err != nil {
 		for index, month := range months {
 			if month == value {
-				return index
+				return index + 1
 			}
 		}
 
